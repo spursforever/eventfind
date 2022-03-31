3b84e72416ff8d2b7cdd0976341fa7a6ac5d1805
Dayton Chen <daytonchen120@gmail.com>
2022-03-31 13:35:58 -0700

    User model and testing User Auth Middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,11 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
 
   
   module.exports = router;
