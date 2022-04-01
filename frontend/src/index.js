bdd6faaaab02f295e690b65341d568b6bd246674
Dayton Chen <daytonchen120@gmail.com>
2022-03-31 17:11:56 -0700

    CsrfFetch setup and bug fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+    restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,56 +1,24 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import React from 'react';
-
-import './index.css';
-
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-import configureStore from './store';
-
-const store = configureStore();
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
-function Root() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <Root />
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+// import sessionReducer from './session';
 
 const rootReducer = combineReducers({
+//   session: sessionReducer,
 });
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
-  };
-  
+  return createStore(rootReducer, preloadedState, enhancer);
+};
 
 export default configureStore;
