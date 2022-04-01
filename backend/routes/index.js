e9fcb2ebac8a681d5520dc0c096cb69e48efa841
Dayton Chen <daytonchen120@gmail.com>
2022-04-01 16:35:11 -0700

    Heroku Setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
+
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
@@ -11,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -32,9 +33,11 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
+
+module.exports = router;
 
 
 
