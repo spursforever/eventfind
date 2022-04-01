e371534fc785ac238d0717c5cef86ccf3e621903
Dayton Chen <daytonchen120@gmail.com>
2022-04-01 11:35:34 -0700

    Sessions actions and reducer setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
+
 
 const store = configureStore();
 
@@ -12,6 +16,17 @@ if (process.env.NODE_ENV !== 'production') {
     restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 ReactDOM.render(
