5134e20f6ea54f9196b740b31db5c2fc5443fd5f
Dayton Chen <daytonchen120@gmail.com>
2022-05-04 19:08:19 -0700

    add ticket seeder file

---
+++ backend/db/seeders/20220505013152-Tickets.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('Tickets', [
+      {
+        eventId: 1,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 10,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 14,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 6,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 2,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 8,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkDelete('Tickets', null, {});
+
+  }
+};
