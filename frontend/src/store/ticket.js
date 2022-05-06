9129836bc284b62ff544e6e2aaf0c0b42cf9c107
Dayton Chen <daytonchen120@gmail.com>
2022-05-05 20:16:15 -0700

    setup backend route and store for delete ticket feature

--- frontend/src/store/ticket.js
+++ frontend/src/store/ticket.js
@@ -3,18 +3,26 @@ import { csrfFetch } from "./csrf";
 //actions
 const ALL_TICKETS = 'ticket/ALL_TICKETS'
 const ADD_TICKET = 'ticket/ADD_TICKET'
+const REMOVE_TICKET = 'ticket/REMOVE_TICKET'
 
 // action creator
 const allTickets = (tickets) => {
     return {
-       type: ALL_TICKETS,
-    tickets 
+        type: ALL_TICKETS,
+        tickets
     }
-    
+
 }
 const addTicket = (ticket) => {
     return {
         type: ADD_TICKET,
+        ticket
+    }
+}
+
+const removeTicket = (ticket) => {
+    return {
+        type: REMOVE_TICKET,
         ticket
     }
 }
@@ -32,7 +40,7 @@ export const getAllTickets = (id) => async dispatch => {
 export const addOneTicket = (data) => async dispatch => {
     const backendResponse = await csrfFetch(`/api/tickets/events/${data.id}`, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
     });
     if (backendResponse.ok) {
@@ -42,19 +50,37 @@ export const addOneTicket = (data) => async dispatch => {
     }
 }
 
+export const deleteTicket = (ticketId) => async dispatch => {
+    const backendResponse = await csrfFetch(`/api/tickets/${ticketId}`, {
+        method: "DELETE",
+    });
+    if (backendResponse.ok) {
+        const ticket = await backendResponse.json();
+        dispatch(removeTicket(ticketId))
+    }
+}
+
 //reducer
 const ticketReducer = (state = {}, action) => {
     let updatedState;
-        switch (action.type) {
+    switch (action.type) {
         case ALL_TICKETS:
-            return {...state, 
-            list :action.tickets}
+            return {
+                ...state,
+                list: action.tickets
+            }
         case ADD_TICKET:
             updatedState = { ...state };
             updatedState[action.event.id] = action.event;
             return updatedState;
-    default:
-        return state;
-}
+        case REMOVE_TICKET:
+            updatedState = { ...state };
+            const filteredTicket = updatedState.list.filter(ticket => (
+                ticket.id !== action.id))
+            updatedState.list = filteredTicket
+            return updatedState;
+        default:
+            return state;
+    }
 }
 export default ticketReducer;
