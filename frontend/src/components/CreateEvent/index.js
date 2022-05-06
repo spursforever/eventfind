823bf5e73be929d544df62bfb3d8702a7fe1a14f
Dayton Chen <daytonchen120@gmail.com>
2022-05-06 14:04:46 -0700

    styling for create event page

--- frontend/src/components/CreateEvent/index.js
+++ frontend/src/components/CreateEvent/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createSingleEvent } from "../../store/event";
 import {useSelector, useDispatch} from "react-redux"
-
+import "./CreateEvent.css"
 const CreateEvent = () => {
     const history = useHistory();
     const user = useSelector((state) => state.session.user);
@@ -49,58 +49,64 @@ const CreateEvent = () => {
           });
     }
 return (
-    <div>
-        <form onSubmit={eventSubmission}>
+    <div className="create_event">
+        <form  className="create_event_form" onSubmit={eventSubmission}>
             <h1>Create your desired Event!</h1>
             <ul>{errors.map((error) => (
-                <li>
+                <li className="error_info">
                 {error}
                 </li>))}
                 </ul>
-                <div>
+                <div className="name">
                     <label>Event Name: </label>
                     <input
                     id="form-label-title"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    className=""
+                    className="name_input"
                     />
                 </div>
-                <div className="">
+                <div className="description">
           <label> Description: </label>
           <textarea
             id="form-label-Date"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className=""
+            className="description_input"
             />
             </div>
           <div>
           <label>ImageUrl: </label>
           <input
+            id="form-label-image-url"
+            className="url_input"
             value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)} />
                   </div>
-          <div>
+          <div className="date">
 
           <label>Date: </label>
           <input
+          className="date_input"
           value={date}
-          onChange={(e) => setDate(e.target.value)}></input>
+          onChange={(e) => setDate(e.target.value)}>             
+            </input>
           </div>
-          <div>
+          <div className="location">
               <label>Location: </label>
-              <input 
+              <input
+              className="location_input" 
               value={location}
               onChange={(e) => setLocation(e.target.value)}
               />
           </div>
                 <button
-                className=""
+                className="submit_event"
                 type="submit"
                 disabled={errors.length >0}
                 >Submit Event</button>
                 <button 
+                className="cancel_event"
                 type="submit"
                 onClick={()=> history.push('/')}> Cancel Event</button>
         </form>
