1263bf4fd54c1fdab98e0f4e43e0f708ee17be34
Dayton Chen <daytonchen120@gmail.com>
2022-05-31 20:01:39 -0700

    starting on aws feature

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const routes = require('./routes');
 const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Security Middleware
